3a3c9f61d7e289812a06771f51a41e1f96ed0d84
BibiFatimaAyaz <93053293+BibiFatimaAyaz@users.noreply.github.com>
2021-12-04 14:59:51 +0500

    Add files via upload

---
+++ hassaaaaaaaan.py
@@ -0,0 +1,28 @@
+##    while True:
+##    number_1 = int(input("Enter first number"))
+##    number_2 = int(input("Enter second number"))
+##    operator = str(input("operator u want to add"))
+##    if operator == "+":
+##        print("The value of addition",number_1+number_2)
+##        break
+##    if operator == "-":
+##        print("The value of subtraction",number_1+number_2)
+##        break
+##    if operator == "*":
+##        print("The value of multiply",number_1+number_2)
+##        break
+##    if operator == "/":
+##        print("The value of division",number_1+number_2)
+##        break
+
+##while True:
+##    animal= int(input("Enter animal name:")
+##    if animal=="Cat":
+##                print("Buy food from cat store")
+##    elif animal=="snake":
+##                print("Buy food from sanle store")
+##    elif animal=="dog":
+##                print("Buy food from dog store")
+##    else animal=="Eagle":
+##                print("Invalid animal")
+##                
